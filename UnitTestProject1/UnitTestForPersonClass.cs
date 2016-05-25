a2cac6170f8e7cfb3d8f6ba4c2b8f2dbbffbb637
kb-ma <kbma.kevin@gmail.com>
2016-05-24 21:57:50 -0400

    added Unit Test Project

---
+++ UnitTestProject1/UnitTestForPersonClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using COMP123_S2016_Lesson3;
+
+namespace UnitTestForLesson3
+{
+    /// <summary>
+    /// Summary description for UnitTestForPersonClass
+    /// </summary>
+    [TestClass]
+    public class UnitTestForPersonClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
